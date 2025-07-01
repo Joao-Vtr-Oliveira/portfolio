c41ac6a83fab26af4c56f2259d9365540630147d
João V. <joao.vtr.oliveira@outlook.com>
2025-07-01 16:46:00 -0300

    feat: improve projects component

--- src/app/home/projects/projects.ts
+++ src/app/home/projects/projects.ts
@@ -2,11 +2,13 @@ import { Component, signal } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import { projects } from './projects-info';
+import {MatIconModule} from '@angular/material/icon';
+import { Icon } from "../../shared/icon/icon";
 
 
 @Component({
   selector: 'app-projects',
-  imports: [MatButtonModule, MatCardModule],
+  imports: [MatButtonModule, MatCardModule, MatIconModule, Icon],
   templateUrl: './projects.html',
   styleUrl: './projects.css'
 })
