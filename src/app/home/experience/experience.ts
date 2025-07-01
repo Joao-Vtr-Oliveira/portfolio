2c8f8e72a34c1ce840390428b2bdc29d7e1fecbd
João V. <joao.vtr.oliveira@outlook.com>
2025-07-01 16:45:51 -0300

    feat: improve experience styling

--- src/app/home/experience/experience.ts
+++ src/app/home/experience/experience.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import { experiences } from './experience-info';
+import {MatIconModule} from '@angular/material/icon';
 
 
 @Component({
   selector: 'app-experience',
-  imports: [MatButtonModule, MatCardModule],
+  imports: [MatButtonModule, MatCardModule, MatIconModule],
   templateUrl: './experience.html',
   styleUrl: './experience.css'
 })
