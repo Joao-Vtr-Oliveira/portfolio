97f2cacf32344b067f7a7094da695a36eb21f80e
João V. <joao.vtr.oliveira@outlook.com>
2025-07-01 10:49:53 -0300

    feat: improve header putting likedin and github

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -3,10 +3,11 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { Router } from '@angular/router';
+import { Icon } from "../shared/icon/icon";
 
 @Component({
   selector: 'app-header',
-  imports: [MatIconModule, MatButtonModule, MatToolbarModule],
+  imports: [MatIconModule, MatButtonModule, MatToolbarModule, Icon],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
