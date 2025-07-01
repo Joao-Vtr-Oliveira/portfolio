bfc62e875835c3204102e74323f8b32ba32fd8c7
João V. <joao.vtr.oliveira@outlook.com>
2025-07-01 15:08:56 -0300

    feat: add projects in the home component

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AboutMe } from "./about-me/about-me";
 import { Introduction } from "./introduction/introduction";
+import { Projects } from "./projects/projects";
 
 @Component({
   selector: 'app-home',
-  imports: [AboutMe, Introduction],
+  imports: [AboutMe, Introduction, Projects],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
