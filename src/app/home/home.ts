24aade894ae73d18d6257b2494628710cf6e7425
João V. <joao.vtr.oliveira@outlook.com>
2025-07-01 17:31:07 -0300

    feat: add contact component

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -4,10 +4,11 @@ import { Introduction } from "./introduction/introduction";
 import { Projects } from "./projects/projects";
 import { Experiences } from "./experience/experience";
 import { TechStackComponent } from "./tech-stack/tech-stack";
+import { ContactComponent } from "./contact-component/contact-component";
 
 @Component({
   selector: 'app-home',
-  imports: [AboutMe, Introduction, Projects, Experiences, TechStackComponent],
+  imports: [AboutMe, Introduction, Projects, Experiences, TechStackComponent, ContactComponent],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
