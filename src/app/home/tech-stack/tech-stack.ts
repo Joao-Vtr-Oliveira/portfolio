43711c8022b24a82b2070eb20aa77a2b78c5a156
João V. <joao.vtr.oliveira@outlook.com>
2025-07-01 17:06:40 -0300

    feat: add tech stack

--- src/app/home/tech-stack/tech-stack.ts
+++ src/app/home/tech-stack/tech-stack.ts
@@ -5,10 +5,35 @@ import { Component } from '@angular/core';
 	templateUrl: './tech-stack.html',
 })
 export class TechStackComponent {
-	frontendIcons =
-		'https://skillicons.dev/icons?i=html,css,js,ts,angular,react,nextjs,vue,vuetify,tailwind,redux&perline=5';
-	backendIcons =
-		'https://skillicons.dev/icons?i=nodejs,express,mongodb,mysql,postgres,sequelize,prisma&perline=5';
-	toolsIcons =
-		'https://skillicons.dev/icons?i=git,github,postman,docker&perline=5';
+	frontendIcons = [
+		{ name: 'HTML', icon: 'html' },
+		{ name: 'CSS', icon: 'css' },
+		{ name: 'JavaScript', icon: 'js' },
+		{ name: 'TypeScript', icon: 'ts' },
+		{ name: 'Angular', icon: 'angular' },
+		{ name: 'React', icon: 'react' },
+		{ name: 'Next.js', icon: 'nextjs' },
+		{ name: 'Vue.js', icon: 'vue' },
+		{ name: 'Vuetify', icon: 'vuetify' },
+		{ name: 'TailwindCSS', icon: 'tailwind' },
+		{ name: 'Redux', icon: 'redux' },
+	];
+
+	backendIcons = [
+		{ name: 'Node.js', icon: 'nodejs' },
+		{ name: 'Express', icon: 'express' },
+		{ name: 'MongoDB', icon: 'mongodb' },
+		{ name: 'MySQL', icon: 'mysql' },
+		{ name: 'PostgreSQL', icon: 'postgres' },
+		{ name: 'Sequelize', icon: 'sequelize' },
+		{ name: 'Prisma', icon: 'prisma' },
+	];
+
+	toolsIcons = [
+		{ name: 'Git', icon: 'git' },
+		{ name: 'GitHub', icon: 'github' },
+		{ name: 'Postman', icon: 'postman' },
+		{ name: 'Docker', icon: 'docker' },
+		{ name: 'Jest', icon: 'jest' },
+	];
 }
