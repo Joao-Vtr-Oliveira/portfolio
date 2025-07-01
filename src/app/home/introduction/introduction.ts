0c43926b46f6210e33796f05f87a5bce2c62ac03
João V. <joao.vtr.oliveira@outlook.com>
2025-07-01 13:39:27 -0300

    feat: add introduction component

---
+++ src/app/home/introduction/introduction.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+
+@Component({
+  selector: 'app-introduction',
+  imports: [MatButtonModule],
+  templateUrl: './introduction.html',
+})
+export class Introduction {
+
+}
