6e4b1c6e05ec925843fbc62659c3d7bf7a4e8ce6
João V. <joao.vtr.oliveira@outlook.com>
2025-07-01 11:37:25 -0300

    test: add test case for header

--- src/app/header/header.spec.ts
+++ src/app/header/header.spec.ts
@@ -1,23 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Header } from './header';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('Header', () => {
-  let component: Header;
-  let fixture: ComponentFixture<Header>;
+	let component: Header;
+	let fixture: ComponentFixture<Header>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [Header]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [Header],
+      providers: [provideHttpClient()]
+		}).compileComponents();
 
-    fixture = TestBed.createComponent(Header);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(Header);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+	it('span should have name', () => {
+		const span: HTMLSpanElement = fixture.nativeElement.querySelector(
+			'[data-testid="spanName-headerComponent"]'
+		);
+    expect(span.textContent).toContain('João Vitor de Oliveira');
+	});
 });
