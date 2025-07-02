526dedf62f71f49611386456815b378a0bae3d94
João V. <joao.vtr.oliveira@outlook.com>
2025-07-02 11:59:34 -0300

    feat: make the projects redirect to repos

--- src/app/home/projects/projects.ts
+++ src/app/home/projects/projects.ts
@@ -1,17 +1,18 @@
 import { Component, signal } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { projects } from './projects-info';
-import {MatIconModule} from '@angular/material/icon';
-import { Icon } from "../../shared/icon/icon";
-
+import { MatIconModule } from '@angular/material/icon';
+import { Icon } from '../../shared/icon/icon';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-projects',
-  imports: [MatButtonModule, MatCardModule, MatIconModule, Icon],
-  templateUrl: './projects.html',
-  styleUrl: './projects.css'
+	selector: 'app-projects',
+	standalone: true,
+	imports: [MatButtonModule, MatCardModule, MatIconModule, Icon, RouterModule],
+	templateUrl: './projects.html',
+	styleUrl: './projects.css',
 })
 export class Projects {
-  projects = signal(projects);
+	projects = signal(projects);
 }
