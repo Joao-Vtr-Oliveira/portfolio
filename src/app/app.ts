2fe02c531c1a20b9a4d44113d027058699468f3f
João V. <joao.vtr.oliveira@outlook.com>
2025-07-02 10:32:00 -0300

    feat: add sidenav

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from "./header/header";
+import { MatSidenavModule, MatSidenav } from '@angular/material/sidenav';
+import { Header } from './header/header';
+import { Icon } from "./shared/icon/icon";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, Header],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+	selector: 'app-root',
+	standalone: true,
+	imports: [RouterOutlet, MatSidenavModule, Header, Icon],
+	templateUrl: './app.html',
+	styleUrl: './app.css',
 })
 export class App {
-  
+	@ViewChild('drawer') drawer!: MatSidenav;
+
+	goTo(route: string) {
+		window.location.href = route;
+	}
+
+	scrollTo(targetId: string) {
+		const target = document.getElementById(targetId);
+		if (target) {
+			target.scrollIntoView({
+				behavior: 'smooth',
+				block: 'start',
+			});
+		}
+		if (this.drawer && this.drawer.opened) {
+			this.drawer.close();
+		}
+	}
 }
--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,31 +1,31 @@
-import { Component, inject } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import { Router } from '@angular/router';
-import { Icon } from "../shared/icon/icon";
+import { Component, Input } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenav } from '@angular/material/sidenav';
+import { Icon } from '../shared/icon/icon';
 
 @Component({
-  selector: 'app-header',
-  imports: [MatIconModule, MatButtonModule, MatToolbarModule, Icon],
-  templateUrl: './header.html',
-  styleUrl: './header.css'
+	selector: 'app-header',
+	standalone: true,
+	imports: [MatToolbarModule, MatIconModule, MatButtonModule, Icon],
+	templateUrl: './header.html',
+	styleUrl: './header.css',
 })
 export class Header {
-  private router = inject(Router);
+	@Input() drawer!: MatSidenav;
 
-  goTo(route: string) {
-    this.router.navigate([route]);
-  }
-
-  scrollTo(targetId: string) {
-	const target = document.getElementById(targetId);
-	if (target) {
-		target.scrollIntoView({
-			behavior: 'smooth',
-			block: 'start',
-		});
+	goTo(route: string) {
+		window.location.href = route;
 	}
-}
 
+	scrollTo(targetId: string) {
+		const target = document.getElementById(targetId);
+		if (target) {
+			target.scrollIntoView({
+				behavior: 'smooth',
+				block: 'start',
+			});
+		}
+	}
 }
