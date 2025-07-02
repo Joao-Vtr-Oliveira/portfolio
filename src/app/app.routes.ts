b61665289ff91693a328be2f11e11e27f8697b10
João V. <joao.vtr.oliveira@outlook.com>
2025-07-02 11:50:39 -0300

    feat: add the repo route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
+import { Repo } from './repo/repo';
 
 export const routes: Routes = [
-  {path: '', component: Home}
+  {path: '', component: Home},
+  {path: 'repo/:repo', component: Repo},
+  {path: '**', redirectTo: ''},
 ];
